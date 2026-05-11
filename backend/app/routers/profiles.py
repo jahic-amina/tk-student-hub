365a043b74c580f8c7d5b6ea455f0d2584283052
hedijasisic <sisichedija23@gmail.com>
2026-05-11 23:55:52 +0200

    Uključene potrebne biblioteke i definisana pravila i putanja za upload slika

--- backend/app/routers/profiles.py
+++ backend/app/routers/profiles.py
@@ -3,23 +3,14 @@ from sqlmodel import Session
 from app.database import get_db
 from app.core.security import get_current_user
 from app.models.user import User
+from uuid import uuid4
+import os
 
 router = APIRouter(prefix="/profiles", tags=["profiles"])
 
-# -------------------------------------------------------
-# Team 4 — Profiles & Dashboard
-# This is your router. All your endpoints go here.
-#
-# Example protected endpoint:
-#
-# @router.get("/")
-# def get_profile(
-#     db: Session = Depends(get_db),
-#     current_user: User = Depends(get_current_user)
-# ):
-#     return {"message": "your code here"}
-#
-# -------------------------------------------------------
+UPLOAD_DIR = "uploads/"
+ALLOWED_IMAGE_TYPES = ["image/jpeg", "image/png"]
+MAX_FILE_SIZE_MB = 5
 
 @router.get("/")
 def profiles_placeholder():
