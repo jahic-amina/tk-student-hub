bae5ada53cee89b518754231e726706c6618dc9f
hedijasisic <sisichedija23@gmail.com>
2026-05-16 16:58:11 +0200

    Napravljena shema za prikaz profila korisnika

--- backend/app/models/profile.py
+++ backend/app/models/profile.py
@@ -3,6 +3,14 @@ from typing import Optional
 from enum import Enum
 from datetime import datetime
 
+class UserProfileResponse(SQLModel):
+    id: int
+    email: str
+    full_name: str
+    role: str
+    created_at: Optional[datetime]
+    profilna_slika_url: Optional[str]
+
 class AvatarUploadResponse(SQLModel):
     profilna_slika_url: str
 
