0d9214c9c91865175d5796aa1c57553ddcf32953
hedijasisic <sisichedija23@gmail.com>
2026-05-17 14:25:11 +0200

    feat: dodane sekcije za trenutne prakse i nedavne aktivnosti

--- frontend/src/views/profiles/ProfilesView.vue
+++ frontend/src/views/profiles/ProfilesView.vue
@@ -11,3 +11,20 @@
         <h2>O meni</h2>
         <p>{{ profile.biografija || 'Nije unesena biografija.' }}</p>
       </div>
+
+       <div>
+          <h2>Trenutne prakse</h2>
+          <p>Nema trenutnih praksi.</p>
+        </div>
+
+        <div>
+          <h2>Nedavna aktivnost</h2>
+          <p>Nema nedavne aktivnosti.</p>
+        </div>
+
+      </div>
+
+    </div>
+
+  </div>
+</template>
