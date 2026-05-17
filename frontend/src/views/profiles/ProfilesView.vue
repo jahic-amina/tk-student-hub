e9d9ad02c6ec92c5f53a17f9a4d4c8b407813433
hedijasisic <sisichedija23@gmail.com>
2026-05-17 14:23:26 +0200

    feat: dodan prikaz korisničkog profila i biografije

--- frontend/src/views/profiles/ProfilesView.vue
+++ frontend/src/views/profiles/ProfilesView.vue
@@ -4,3 +4,10 @@
     <div v-if="loading"> Učitavanje...</div>
 
     <div v-else-if="error"> {{ error }} </div>
+    
+    <div v-else-if="profile">
+      <UserProfileCard :profile="profile" />
+      <div>
+        <h2>O meni</h2>
+        <p>{{ profile.biografija || 'Nije unesena biografija.' }}</p>
+      </div>
