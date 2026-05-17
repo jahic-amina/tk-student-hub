abb559377a414e34b3c8fff13537002db7cdf1ef
Faris Ćosić <faris.cosic@fet.ba>
2026-05-17 17:18:45 +0200

    Import MaterialDetail

--- frontend/src/views/materials/MaterialsView.vue
+++ frontend/src/views/materials/MaterialsView.vue
@@ -1,10 +1,20 @@
 <template>
   <div class="flex gap-6">
-    <MaterialList />
+    <MaterialList @open="openMaterial" />
+
+    <MaterialDetail v-if="selectedMaterialId" :material="selectedMaterialId" @close="selectedMaterialId = null" />
   </div>
 </template>
 
 <script setup>
 import { ref } from 'vue'
 import MaterialList from '../../components/MaterialList.vue'
+import MaterialDetail from '../../components/MaterialDetail.vue'
+import { getMaterial } from '../../services/api'
+
+const selectedMaterialId = ref(null)
+
+async function openMaterial(id) {
+  selectedMaterialId.value = await getMaterial(id)
+}
 </script>
