db141bea91706aba9c7de1a4a89901e17062823d
Tahira <taha.zuka@gmail.com>
2026-05-18 17:49:21 +0200

    Importovani fajlovi OglasiPage i OglasCard

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import LoginView from '../views/LoginView.vue'
 import RegisterView from '../views/RegisterView.vue'
-import OglasiPage from '../components/OglasiPage.vue'
+import OglasiPage, OglasCard '../components/OglasiPage.vue', '../components/OglasCard.vue'
 
 const routes = [
   {
