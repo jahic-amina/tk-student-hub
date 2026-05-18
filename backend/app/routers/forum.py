eca26a1ee795cc16bef1bad070f1d79c1c62f762
amarascic <amar.ascic@fet.ba>
2026-05-18 19:55:15 +0200

    Dodan endpoint za dohvat kategorija i broja tema za svaku kategoriju

--- backend/app/routers/forum.py
+++ backend/app/routers/forum.py
@@ -1,26 +1,46 @@
 from fastapi import APIRouter, Depends
-from sqlmodel import Session
+from sqlmodel import Session, select, func
+from typing import List, Dict, Any
+
 from app.database import get_db
 from app.core.security import get_current_user
 from app.models.user import User
 
+from app.models.forum import ForumCategory, ForumTopic
+
 router = APIRouter(prefix="/forum", tags=["forum"])
 
-# -------------------------------------------------------
-# Team 3 — Forum
-# This is your router. All your endpoints go here.
-#
-# Example protected endpoint:
-#
-# @router.get("/")
-# def get_posts(
-#     db: Session = Depends(get_db),
-#     current_user: User = Depends(get_current_user)
-# ):
-#     return {"message": "your code here"}
-#
-# -------------------------------------------------------
+@router.get("/categories", response_model=List[Dict[str, Any]])
 
-@router.get("/")
-def forum_placeholder():
-    return {"message": "Forum router is working — Team 3 builds here"}
+#Dohvat kategorija i broja tema za svaku kategoriju
+
+@router.get("/categories", response_model=List[Dict[str, Any]])
+def get_categories_with_topic_count(db: Session = Depends(get_db)):
+
+    statement = (
+        select(
+            ForumCategory.id,
+            ForumCategory.name,
+            ForumCategory.color,
+            ForumCategory.description,
+            func.count(ForumTopic.id).label("topic_count")
+        )
+        .join(ForumTopic, ForumTopic.category_id == ForumCategory.id, isouter=True)
+
+        .where((ForumTopic.is_deleted == False) | (ForumTopic.id == None))
+        .group_by(ForumCategory.id)
+    )
+    
+    results = db.exec(statement).all()
+    
+    output = []
+    for row in results:
+        output.append({
+            "id": row.id,
+            "name": row.name,
+            "color": row.color,
+            "description": row.description,
+            "topic_count": row.topic_count
+        })
+        
+    return output
