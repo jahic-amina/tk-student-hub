068469b0b651ad2d08afb20f5499b1a658566d64
Iman Osmic <iman.osmic@fet.ba>
2026-06-14 22:11:08 +0200

    Dodane migracije za reputaciju i medalje

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -18,6 +18,7 @@ import app.models.application  # noqa: F401
 import app.models.notification  # noqa: F401
 import app.models.ad_bookmark  # noqa: F401
 import app.models.forum  # noqa: F401
+import app.models.forum_reputation  # noqa: F401
 
 config = context.config
 
