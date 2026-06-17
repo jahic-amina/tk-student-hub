7d2d06411a6ab4eac279aa1e00ec19b21a4fee8b
Marinela Mitic <marinela.mitic@fet.ba>
2026-06-17 04:05:05 +0200

    feat: prikaži thumbnail prve stranice PDF-a u listi materijala

--- frontend/src/components/MaterialCard.vue
+++ frontend/src/components/MaterialCard.vue
@@ -29,17 +29,24 @@
 </button>
 
         <div class="flex items-start gap-4 flex-1 cursor-pointer" @click="$emit('click', material.id)">
-            <div class="bg-red-100 text-red-500 p-3 rounded-lg shrink-0">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-                    stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                    class="lucide lucide-file-text w-8 h-8 text-destructive">
-                    <path d="M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7Z"></path>
-                    <path d="M14 2v4a2 2 0 0 0 2 2h4"></path>
-                    <path d="M10 9H8"></path>
-                    <path d="M16 13H8"></path>
-                    <path d="M16 17H8"></path>
-                </svg>
-            </div>
+           <div class="bg-red-100 text-red-500 p-3 rounded-lg shrink-0">
+    <img 
+        v-if="material.thumbnail_path" 
+        :src="`http://127.0.0.1:8000/thumbnails/${material.thumbnail_path.split('/').pop()}`"
+        class="w-16 h-16 object-cover rounded"
+        alt="thumbnail"
+    />
+    <svg v-else xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+        stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+        class="lucide lucide-file-text w-8 h-8 text-destructive">
+        <path d="M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7Z"></path>
+        <path d="M14 2v4a2 2 0 0 0 2 2h4"></path>
+        <path d="M10 9H8"></path>
+        <path d="M16 13H8"></path>
+        <path d="M16 17H8"></path>
+    </svg>
+</div>
+            
             <div>
                 <p class="font-semibold text-gray-800">{{ material.title }}</p>
                 <p class="text-sm text-gray-500">{{ material.description }}</p>
